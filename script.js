ea4d42dd59f24f80361e12f285112b304ce4fb56
Jhonah <buryakone@gmail.com>
2021-06-30 11:14:57 +0700

    сделал как в задание через указание нуля в параметрах где указывается день

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
 function getLastDayOfMonth(year, month) {
-
-    let date = new Date(year, month);    
-
-    date.setDate(date.getDate() + 29);
-
-    if (!(date.getMonth() == month)) {
-        if (year % 4 == 0) return "29 (высокосный год, февраль)";
-        return "28 (невысокосный год, февраль)";
-    }
-    
-    return ((date.getDate() + 1) == month) ? 31 : 30;
+    let date = new Date(year, month + 1, 0);
+    return date.getDate();
 }
 
-alert(getLastDayOfMonth(2007, 1));
-alert(new Date(2007, 1, 28));
+alert(getLastDayOfMonth(2007, 5)); // 30
+alert(getLastDayOfMonth(2013, 0)); // 31
+alert(getLastDayOfMonth(2021, 1)); // 28
